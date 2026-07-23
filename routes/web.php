82a2985dbc1f7dce46e3463a0bfacddec8c4fa9e
Vinows <vincentwilliam2009@gmail.com>
2026-07-23 10:03:34 +0700

    feat: resource routes for teachers
    Introduces a complete set of CRUD routes for managing teacher resources. This mirrors the existing student management, providing endpoints for listing, viewing, creating, updating, and deleting teacher data.
    
    Also refines the response messages for student-related routes for improved clarity.

--- routes/web.php
+++ routes/web.php
@@ -13,26 +13,56 @@ Route::prefix('students')->name('students.')->group(function () {
     })->name('index');
 
     Route::get('/{id}', function($id){
-        return "Menampilkan detail siswa dengan ID: {$id}";
+        return "Menampilkan siswa dengan ID: {$id}";
     })->name('show');
 
     Route::get('/create', function(){
-        return "Halaman ini menampilkan halaman tambah siswa";
+        return "Menampilkan halaman tambah siswa";
     })->name('create');
 
     Route::get('/{id}/edit', function(string $id){
-        return "Halaman ini menampilkan halaman edit siswa dengan ID: {$id}";
+        return "Menampilkan halaman edit siswa dengan ID: {$id}";
     })->name('edit');
 
     Route::post('/', function(){
-        return "Menambah data siswa baru";
+        return "Melakukan penambahan data siswa";
     })->name('store');
 
     Route::put('/{id}', function(string $id){
-        return "Mengubah data siswa dengan ID: {$id}";
+        return "Melakukan perubahan data siswa dengan ID: {$id}";
     })->name('edit');
 
     Route::delete('/{id}', function(string $id){
         return "Menghapus data siswa dengan ID: {$id}";
+    })->name('destroy');
+});
+
+Route::prefix('teachers')->name('teachers.')->group(function () {
+    Route::get('/', function () {
+        return "Ini adalah halaman daftar guru";
+    })->name('index');
+
+    Route::get('/{id}', function($id){
+        return "Menampilkan guru dengan ID: {$id}";
+    })->name('show');
+
+    Route::get('/create', function(){
+        return "Menampilkan halaman tambah guru";
+    })->name('create');
+
+    Route::get('/{id}/edit', function(string $id){
+        return "Menampilkan halaman edit guru dengan ID: {$id}";
+    })->name('edit');
+
+    Route::post('/', function(){
+        return "Melakukan penambahan data guru";
+    })->name('store');
+
+    Route::put('/{id}', function(string $id){
+        return "Melakukan perubahan data guru dengan ID: {$id}";
+    })->name('edit');
+
+    Route::delete('/{id}', function(string $id){
+        return "Menghapus data guru dengan ID: {$id}";
     })->name('destroy');
 });
